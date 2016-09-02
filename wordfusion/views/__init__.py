0f17652ee3ea14575d45602ebe979f49dac32d67
Krerkkiat Chusap <chusap.krerkkiat@gmail.com>
2016-09-02 13:18:32 -0400

    Edit word_fusio.html to be able to cache the result that is not a word to reduce the number of request going to server. Add /search to support query for a word that is not belong to the set. Add link to play the set at the set-listing level.

--- wordfusion/views/__init__.py
+++ wordfusion/views/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from wordfusion import app
-from wordfusion.models import Set
+from wordfusion.models import CompoundWord
 from wordfusion.database import db_session
 from wordfusion.views.session import *
 from wordfusion.views.set import *
@@ -10,3 +10,19 @@ from wordfusion.views.set import *
 def index():
     '''Show the index page.'''
     return 'Word Fusion'
+
+@app.route('/search/<word>')
+def search(word):
+    words = db_session.query(CompoundWord).filter(CompoundWord.combination == word).all()
+
+    res = []
+    for word in words:
+        obj = {}
+        obj['full_word'] = word.combination
+        obj['description'] = word.description
+        res.append(obj)
+
+    response = make_response(json.dumps(res), 200)
+    response.headers['Content-Type'] = 'text/json; charset=utf-8'
+
+    return response
