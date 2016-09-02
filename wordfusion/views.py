4ea28681fd17b218d57d452bec7125794616e88b
Krerkkiat Chusap <chusap.krerkkiat@gmail.com>
2016-09-01 22:18:25 -0400

    Add Node and npm info for Dragula and jQuery. Add sample routes and templates for sessions, a session, words in set, html page for the wordfusioning. Integrate the wordfusion prototype by editing it to use jQuery to fetch the words on the fly. Also alter the prototype to use Jinja2 template and url_for. Update layout.html to have more tags and config the default zoom. In addition, I also add more block since the prototype use some other block than the body. Add node_modules into .gitignore.

--- wordfusion/views.py
+++ wordfusion/views.py
@@ -1,7 +1,47 @@
 # -*- coding: utf-8 -*-
+import json
+
+from flask import render_template, make_response
 
 from wordfusion import app
+from wordfusion.models import Set, Session
+from wordfusion.database import db_session
 
 @app.route('/')
 def index():
+    '''Show the index page.'''
     return 'Word Fusion'
+
+@app.route('/sessions')
+def show_session_list():
+    '''Show the list of sessions.'''
+    session_list = db_session.query(Session).all()
+    return render_template('list_sessions.html', session_list=session_list)
+
+@app.route('/session/<int:session_id>')
+def show_session(session_id):
+    '''Show information of the session.'''
+    session_object = db_session.query(Session).filter(Session.id == session_id).first()
+    return render_template('show_session.html', session_object=session_object)
+
+@app.route('/words/<int:set_id>')
+def get_words_in_set(set_id):
+    '''Show all words in a set in json format.'''
+    set_object = db_session.query(Set).filter(Set.id == set_id).first()
+
+    words_list = []
+    for word in set_object.compound_words:
+        word_obj = {}
+        word_obj['full_word'] = word.combination
+        word_obj['description'] = word.description
+        words_list.append(word_obj)
+
+    response = make_response(json.dumps(words_list), 200)
+    response.headers['Content-Type'] = 'text/json; charset=utf-8'
+
+    return response
+
+@app.route('/wordfusion', methods=['GET'])
+def wordfusion():
+    '''Serve the html for the wordfusion page.'''
+    return render_template('word_fusion.html')
