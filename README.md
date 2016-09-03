c4dd2480d2ce45a6fb9048b7b5fbbeda9e85dee8
Krerkkiat Chusap <chusap.krerkkiat@gmail.com>
2016-09-03 17:16:20 -0400

    Add info and include seeding code Add installation and running information to README.md. Add seeding module and sample data into version control.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Word Fusion
+
+## Installation
+Run `pip install -r requirements.txt`.
+Run `npm install` then copy `node_modules` to `wordfusion/static`.
+
+## Database seeding
+Run `python seeding.database_seeding`.
+
+## Run the server
+`python runserver.py`
---
+++ seeding/database_seeding.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+from wordfusion.database import db_session
+from wordfusion.models import *
+
+def seed_db():
+    from wordfusion.models import User, Set, CompoundWord
+    from wordfusion.database import db_session
+
+    user = User(username='foobar', first_name='Foo', last_name='bar')
+    db_session.add(user)
+    db_session.commit()
+
+    with open('seeding/seed_data.json') as f:
+        data = json.load(f)
+
+        if 'sets' in data.keys():
+            for s in data['sets']:
+                set = Set(name=s['name'], description=s['description'])
+                for cw in s['compound_words']:
+                    compound_word = CompoundWord(text_th=cw['text_th'],\
+                                                 text_eng=cw['text_eng'],\
+                                                 description=cw['description'],\
+                                                 part_of_speech=cw['part_of_speech'],\
+                                                 combination=cw['combination'])
+
+                    set.compound_words.append(compound_word)
+
+                db_session.add(set)
+
+            db_session.commit()
+        if 'sessions' in data.keys():
+            for s in data['sessions']:
+                session_object = Session(name=s['name'],\
+                                         description=s['description'],\
+                                         condition='test condition',\
+                                         active=s['active'])
+                #
+                db_session.add(session_object)
+            db_session.commit()
+
+if __name__ == '__main__':
+    from wordfusion.database import init_db
+
+    init_db()
+    seed_db()
